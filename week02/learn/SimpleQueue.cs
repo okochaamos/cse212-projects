a3cda5b4ab3f3980c07c615bbeed52e72fe015e9
okochaamos <oko23015@byui.edu>
2025-07-14 02:55:44 -0700

    week 02 learning

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -1,40 +1,28 @@
-﻿public class SimpleQueue {
-    public static void Run() {
-        // Test Cases
+﻿using System;
+using System.Collections.Generic;
 
+public class SimpleQueue {
+    public static void Run() {
         // Test 1
-        // Scenario: Enqueue one value and then Dequeue it.
-        // Expected Result: It should display 100
         Console.WriteLine("Test 1");
         var queue = new SimpleQueue();
         queue.Enqueue(100);
         var value = queue.Dequeue();
-        Console.WriteLine(value);
-        // Defect(s) Found:
-
+        Console.WriteLine(value); 
         Console.WriteLine("------------");
 
         // Test 2
-        // Scenario: Enqueue multiple values and then Dequeue all of them
-        // Expected Result: It should display 200, then 300, then 400 in that order
         Console.WriteLine("Test 2");
         queue = new SimpleQueue();
         queue.Enqueue(200);
         queue.Enqueue(300);
         queue.Enqueue(400);
-        value = queue.Dequeue();
-        Console.WriteLine(value);
-        value = queue.Dequeue();
-        Console.WriteLine(value);
-        value = queue.Dequeue();
-        Console.WriteLine(value);
-        // Defect(s) Found: 
-
+        Console.WriteLine(queue.Dequeue()); 
+        Console.WriteLine(queue.Dequeue()); 
+        Console.WriteLine(queue.Dequeue()); 
         Console.WriteLine("------------");
 
         // Test 3
-        // Scenario: Dequeue from an empty Queue
-        // Expected Result: An exception should be raised
         Console.WriteLine("Test 3");
         queue = new SimpleQueue();
         try {
@@ -44,30 +32,20 @@
         catch (IndexOutOfRangeException) {
             Console.WriteLine("I got the exception as expected.");
         }
-        // Defect(s) Found: 
     }
 
     private readonly List<int> _queue = new();
 
-    /// <summary>
-    /// Enqueue the value provided into the queue
-    /// </summary>
-    /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        _queue.Insert(0, value); // 
     }
 
-    /// <summary>
-    /// Dequeue the next value and return it
-    /// </summary>
-    /// <exception cref="IndexOutOfRangeException">If queue is empty</exception>
-    /// <returns>First integer in the queue</returns>
     private int Dequeue() {
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[^1]; // Last item = first enqueued
+        _queue.RemoveAt(_queue.Count - 1);
         return value;
     }
 }
